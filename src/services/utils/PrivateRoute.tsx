acd7caf24bec4ba76963235bbb1e0ca4342d4f64
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-04-24 19:54:44 +0200

    added notfoundpage.tsx and privateroute.tsx to the project, and updated the app.tsx file with some routes

---
+++ src/services/utils/PrivateRoute.tsx
@@ -0,0 +1,7 @@
+import { Outlet, Navigate} from 'react-router-dom';
+
+export default function PrivateRoute(){
+    const auth = { token: true};
+
+    return auth.token? <Outlet /> : <Navigate to="/" /> //? <Outlet /> est un composant qui correspond à toutes les routes excepté la page d'accueil "/"
+}
