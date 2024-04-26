f1366ead36a8d148e5b29e8788049d426f0e32c3
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-04-26 10:47:42 +0200

    corresponding route for UserEditProfilePage added to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NotFoundPage from './services/utils/NotFoundPage';
 import PrivateRoute from './services/utils/PrivateRoute';
+import UserEditProfilePage from './pages/User/UserEditProfilePage';
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
           <Route path="/" />
 
             <Route element={ <PrivateRoute /> }>
-
+              <Route path='/editer-mon-profil' element={ <UserEditProfilePage /> } />
             </Route>
 
           <Route path="*" element= { <NotFoundPage />} />
