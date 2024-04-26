fdb713562e2046d5732abc8d82a377807672bd60
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-04-26 10:43:13 +0200

    corresponding route added to the app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NotFoundPage from './services/utils/NotFoundPage';
 import PrivateRoute from './services/utils/PrivateRoute';
+import UserProfilePage from './pages/User/UserProfilePage';
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
           <Route path="/" />
 
             <Route element={ <PrivateRoute /> }>
-
+              <Route path='/mon-compte' element={ <UserProfilePage /> } />
             </Route>
 
           <Route path="*" element= { <NotFoundPage />} />
