c7f2e1134b9504df478ced2d9abc43a9256b8fdf
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-04-30 06:21:27 +0200

    added flowbite/tailwind to UserProfilePage.tsx

--- src/pages/User/UserProfilePage.tsx
+++ src/pages/User/UserProfilePage.tsx
@@ -1,35 +1,58 @@
-
+import { Button, Modal, Card } from "flowbite-react";
+import { useState } from "react";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 export default function UserProfilePage() {
 
+    const [openModal, setOpenModal] = useState(true);
+
     return (
         <>
-            <div>Compte</div>
+            <h1 className="text-2xl mb-8 font-h1">Compte</h1>
 
-            <div>
-                <ul>
-                    <li><a href="">Déposer une annonce <span>&gt;</span></a></li>
-                    <li><a href="">Voir mes annonces <span>&gt;</span></a></li>
-                    <li><a href="">Voir mes inscriptions <span>&gt;</span></a></li>
-                </ul>
-            </div>
+            <div className="flex justify-center items-center">
+                <Card className="w-96 bg-gray-50 shadow-lg m-8">
+                    <div>
+                        <ul>
+                            <li><a href="">Déposer une annonce <span>&gt;</span></a></li>
+                            <li><a href="">Voir mes annonces <span>&gt;</span></a></li>
+                            <li><a href="">Voir mes inscriptions <span>&gt;</span></a></li>
+                        </ul>
+                    </div>
 
-            <button id="accountClosureBtn">Demande de fermeture de compte</button>
+                    <Button
+                        className="flex justify-center items-center"
+                        onClick={() => setOpenModal(true)}
+                    >
+                        Fermer mon compte
+                    </Button>
 
-            <div id="accountClosureModal" className="accountClosureModal">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <span className="close">&times;</span>
-                        <h2>Fermer mon compte</h2>
-                    </div>
-                    <div className="modal-body">
-                        <p>Êtes-vous sûr vouloir fermer votre compte</p>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-success">Oui</button>
-                        <button type="button" className="btn btn-warning" data-dismiss="modal">Non</button>
-                    </div>
-                </div>
+                    <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
+                        <Modal.Header>Fermeture de compte</Modal.Header>
+                        <Modal.Body>
+                            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                            <div className="space-y-6">
+                                <h3 className="text-base leading-relaxed text-gray-500 dark:text-gray-400 text-center font-h3">
+                                Êtes-vous sûr de vouloir fermer votre compte ?
+                                </h3>
+                            </div>
+                            <div className="flex justify-center gap-4 mt-4">
+                                <Button
+                                    color="success"
+                                    onClick={() => setOpenModal(false)}
+                                >
+                                    Oui
+                                </Button>
+                                <Button
+                                    color="failure"
+                                    onClick={() => setOpenModal(false)}
+                                >
+                                    Non
+                                </Button>
+                            </div>
+                        </Modal.Body>
+                    </Modal>                    
+                </Card>
             </div>
         </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ export default {
     flowbite.content(),
   ],
   theme: {
+    fontFamily: {
+      navbar: ['Homemade Apple'],
+      h1: ['Kalam'],
+      h3: ['Kalam'],
+      input: ['Kalam'],
+      button: ['Kalam'],
+      footer: ['Kalam'],
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+    },
     extend: {},
   },
   plugins: [
