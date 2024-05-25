3cdf2596746a20e7529d756e3c7a48fdc8d2f86f
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-05-25 06:56:31 +0200

    updated rendered event content display

--- src/components/Calendar/RenderEventContent.tsx
+++ src/components/Calendar/RenderEventContent.tsx
@@ -15,7 +15,7 @@ export default function RenderEventContent({ eventInfo }: RenderEventContentProp
 
     return (
         <>
-            <span className='flex flex-col text-xs text-white w-72 py-1 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-orange-400 relative inline-block' >
+            <span className='flex flex-col text-xs text-white w-72 py-1 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-orange-400 relative inline-block my-2' >
                 <span className='relative text-white flex flex-col'>
                     <b>{timeText}</b>
                     <i>{event?.title}</i>
