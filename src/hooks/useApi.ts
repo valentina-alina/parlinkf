d18baafac68b344b40d6a4279374aa1e1bce5bf2
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-07-03 10:17:48 +0200

    updated useApi.ts

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -10,7 +10,7 @@ export function useApi() {
     });
 
     api.interceptors.request.use((config) => {
-        const token = localStorage.getItem("access_token");
+        const token = localStorage.getItem("accessToken");
 
         // token ? config.headers["Authorization"] = "Bearer" + token: '';
         if (token) {
