31739465defab64e0dd40e8be86328a6f168adad
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-07-07 14:27:32 +0200

    updated current branch with some testing logic and settings

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,12 +1,16 @@
 describe('Should display my website', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:5173/login');
+    cy.visit('/login');
   })
   it('Should display content CONNECTEZ-VOUS', () => {
     cy.contains(/Connectez-vous/);
+
+    cy.wait(20000);
   })
 
   it('Should login title exist', () => {
-    cy.get('[data-cy="cypress-title"]').should('exist');
+    cy.get('[data-cy="cypress-title"]').should('be.visible');
+
+    cy.wait(20000);
   })
 })
