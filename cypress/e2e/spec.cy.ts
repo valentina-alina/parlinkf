5d9f227724e55b503d3934124b415e9b6a52227b
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-07-07 14:32:57 +0200

    further updated testing logic and settings

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -3,12 +3,14 @@ describe('Should display my website', () => {
     cy.visit('/login');
   })
   it('Should display content CONNECTEZ-VOUS', () => {
+    cy.log('Checking if content is displayed');
     cy.contains(/Connectez-vous/);
 
     cy.wait(20000);
   })
 
-  it('Should login title exist', () => {
+  it('Should login title be visible', () => {
+    cy.log('Checking login title is visible');
     cy.get('[data-cy="cypress-title"]').should('be.visible');
 
     cy.wait(20000);
