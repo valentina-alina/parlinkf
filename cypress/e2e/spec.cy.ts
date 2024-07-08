bac87606ea12c47f52505ac98d3a938ea616047c
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-07-08 19:18:45 +0200

    updated spec.cy.ts

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -16,7 +16,7 @@ describe('Should display my website', () => {
 
   it('Should login title be visible', () => {
     cy.log('Checking login title is visible');
-    cy.get('[data-cy="cypress-title"]').should('be.visible');
+    cy.get('[data-cy="cypress-title"]').should('be.visible').should('have.text', 'Connectez-vous');
 
     cy.wait(20000);
   })
@@ -28,18 +28,18 @@ describe('Should display my website', () => {
     cy.get('input[role="password"').type('password');
 
     cy.get('[data-cy="login"]').click();
+    cy.url().should('match', /\/ads-list$/);
 
     cy.wait(20000);
   })
 
-  it('Should redirect to login', () => {
+  it('Should fail to login', () => {
     cy.log('Checking form data and click');
 
     cy.get('input[role="email"').type(email);
     cy.get('input[role="password"').type(password);
 
     cy.get('[data-cy="login"]').click();
-    cy.url().should('include', '/login')
 
     cy.wait(20000);
   })
