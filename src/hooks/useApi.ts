b63f5f2602c73261b4332ba558d200750ef1432d
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-07-16 16:40:12 +0200

    updated useApi.ts with interceptors.response

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios, { AxiosInstance } from 'axios';
 
 export function useApi() {
@@ -12,12 +13,56 @@ export function useApi() {
     api.interceptors.request.use((config) => {
         const token = localStorage.getItem("accessToken");
 
-        // token ? config.headers["Authorization"] = "Bearer" + token: '';
         if (token) {
             config.headers["Authorization"] = `Bearer ${token}`;
         }
         return config;
     });
 
+    api.interceptors.response.use(
+        (response:any) => response,
+        async (error:any) => {
+            if(error.response && error.response.status === 401) {
+                const originalRequest = error.config;
+
+                if(!originalRequest._retry) {
+                    originalRequest._retry = true;
+                }
+
+                const refreshToken = localStorage.getItem("refreshToken");
+
+                if(refreshToken) {
+                    try {
+                        const result = await refreshAutToken(refreshToken);
+
+                        localStorage.setItem("accessToken", result.data.tokens.accessToken);
+                        localStorage.setItem("refreshToken", result.data.tokens.refreshToken);
+
+                        originalRequest.headers['Authorization'] = 'Bearer' + result.data.tokens.accesToken;
+
+                        return axios(originalRequest);
+                    } catch(error) {
+                        location.href = "/"
+                    }
+                } else {
+                    location.href = "/"
+                }
+            }
+
+            if(error.response && error.response.status === 500) {
+                location.href = "/"
+            }
+
+            return Promise.reject(error);
+        }
+    )
+
     return api;
+}
+
+async function refreshAutToken(refreshToken: string) {
+    const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/auth/refresh_token`, {
+        refreshToken: refreshToken
+    });
+    return response;
 }
