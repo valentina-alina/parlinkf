744a3caf2c6983598eaccd7890dc2533f997d37e
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-07-16 11:06:05 +0200

    updated MapButton.tsx

--- src/components/Map/MapButton.tsx
+++ src/components/Map/MapButton.tsx
@@ -5,7 +5,7 @@ import { FaMapMarkedAlt } from "react-icons/fa";
 export default function MapButton() {
     return (
         <>
-            <div className="fixed bottom-1 left-1/2 z-50 px-2 content-center rounded bg-orange-400 max-sm:hidden py-2">
+            <div className="fixed bottom-1 left-1/2 z-50 px-2 content-center rounded bg-orange-400 max-sm:hidden px-4 py-4">
                 <Link data-cy="map" className='text-md flex gap-2 content-center items-center text-white' to="/map">Voir sur la carte
                     <FaMapMarkedAlt className='h-6 w-6' />
                 </Link>
